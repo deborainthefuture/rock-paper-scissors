3e25b0c166aa7d33199324e660510792f02d89c5
Debora Oliveira <134223070+deborainthefuture@users.noreply.github.com>
2024-02-13 13:29:36 -0300

    Conclude game project page

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
-function getComputerChoice(array) {
-    const word = Math.floor(Math.random() * array.length);
-    return array[word].toLowerCase();
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choice = Math.floor(Math.random() * choices.length);
+
+    return choices[choice].toLowerCase();
 }
 
-const wordsGame = [
-    'Rock',
-    'Paper',
-    'Scissors'
-];
+let player = 0;
+let machine = 0;
+let roundCounter = 0;
+
+function displayMessage(msg) {
+    const message = document.getElementById('game-msg');
+    message.textContent = msg;
+}
+
+function updateScoreCount() {
+    const scoreCount = document.getElementById("score");
+    scoreCount.textContent = `Player: ${player}, Machine: ${machine}`;
+}
+
+function updateRoundCount() {
+    const rounds = document.getElementById("round-counter");
+    rounds.textContent = `Round: ${roundCounter}`
+}
+
+const buttons = document.querySelectorAll(" button");
+buttons.forEach(btn => btn.addEventListener("click", function() {
+    if(roundCounter < 5) {
+        const playerChoice = this.id;
+        const computerChoice = getComputerChoice();
+        console.log(computerChoice);
+
+        const result = playRound(playerChoice, computerChoice);
+
+        let roundMessage = "";
+        if(result === 1) {
+            player++;
+            roundMessage = "You won this round!";
+        } else if(result === -1) {
+            machine++;
+            roundMessage = "Machine won this round!";
+        } else {
+            roundMessage = "Oops! It's a tie!";
+        }
+        roundCounter++;
+        displayMessage(roundMessage);
+        updateScoreCount();
+        updateRoundCount();
+        checkWinner();
+    } else {
+        displayMessage("Game over. Refresh the page to start a new round.")
+    }
+}));
 
 function playRound(playerSelection, computerSelection) {
     if(playerSelection === computerSelection) {
-        return 0; 
+        return 0;
     } else if (
         (playerSelection === 'rock' && computerSelection === 'scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
@@ -23,35 +67,14 @@ function playRound(playerSelection, computerSelection) {
         }
 }
 
-function playGame(n) {
-    let player = 0;
-    let machine = 0;
-
-    for(let i = 0; i < n; i++) {
-        let computerChoice = getComputerChoice(wordsGame);
-        console.log(computerChoice);
-        let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-
-        let result = playRound(playerChoice, computerChoice);
-
-        if(result === 1) {
-            player++;
-            alert("You won this round!");
-        } else if(result === -1) {
-            machine++;
-            alert("Machine won this round!")
+function checkWinner() {
+    if(roundCounter === 6) {
+        if(player > machine) {
+            displayMessage("You won the game!");
+        } else if (machine > player) {
+            displayMessage("Sorry, machine won the game!");
         } else {
-            alert("Oops! It's a tie!");
+            displayMessage("The game ended in a tie!");
         }
     }
-
-    if(player > machine) {
-        alert("You won the game!");
-    } else if (machine > player) {
-        alert("Sorry, machine won the game!");
-    } else {
-        alert("The game ended in a tie!");
-    }
 }
-
-playGame(5);
